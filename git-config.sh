f8fea9531f4118437d1b8dae20de5a3a31397f8b
Claudio Walser <info@gitcd.io>
2022-09-18 14:59:46 +0200

    git config

---
+++ git-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.email "info@gitcd.io"
+git config --global user.name "Claudio Walser"
+
+git config --global user.signingkey "5638393858B04300"
+git config --global commit.gpgsign true
