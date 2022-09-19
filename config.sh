6587fbb25bb42c9f47b66d94ccee214261b45e3e
Claudio <info@gitcd.io>
2022-09-19 08:00:23 +0200

    using env variables between scripts

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-PGP_KEY_ID="5638393858B04300"
-PGP_KEY_EMAIL="info@gitcd.io"
-USER_NAME="Claudio Walser"
+export PGP_KEY_ID="5638393858B04300"
+export PGP_KEY_EMAIL="info@gitcd.io"
+export USER_NAME="Claudio Walser"
--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ printf 'Proceed doing your git config and sign everything (y/n)? '
 read git_config
 
 if echo "$git_config" | grep -iq "^y" ;then
-    echo "doit"
-    . ./git-config.sh
+    echo "Configuring git"
+    bash -ex ./git-config.sh
 fi
+
 echo "done"
