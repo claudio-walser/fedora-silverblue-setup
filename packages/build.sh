412ad591013d8bab9ad4913cd7641cca92c38877
Claudio <info@gitcd.io>
2022-09-26 19:21:48 +0200

    Moved after install because of silverblue restrictions

--- packages/build.sh
+++ packages/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd fix-sudo
-fpm -s dir --after-install after-install.sh -t rpm -n fix-sudo -v 0.0.1 -C ./
+fpm -s dir --after-install etc/after-install.sh -t rpm -n fix-sudo -v 0.0.1 -C ./
 cd -
