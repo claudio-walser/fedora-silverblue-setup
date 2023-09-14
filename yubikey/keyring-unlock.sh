16836a63225689841954377392fe3c90e1eb2102
Claudio Walser <info@gitcd.io>
2023-09-14 19:57:23 +0200

    Create pre-requisite directories

--- yubikey/keyring-unlock.sh
+++ yubikey/keyring-unlock.sh
@@ -1,4 +1,8 @@
 set -o nounset # exit when trying to use undeclared variables
 
+# create pre-requisite directories
+mkdir -p $HOME/.sec
+mkdir -p $HOME/.config/systemd/user/
+
 pip install --user --upgrade gnome-keyring-gpg-unlock
 gnome-keyring-gpg-unlock setup --public-key $PGP_KEY_ID --secret $HOME/.sec/gnome-keyring
