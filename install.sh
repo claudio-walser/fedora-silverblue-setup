fd3ca3aa28aaeaa970200bee63536963525cbb82
Claudio <info@gitcd.io>
2023-10-30 16:11:47 +0100

    Changed ordering in order to work

--- install.sh
+++ install.sh
@@ -2,28 +2,10 @@
 
 . ./config.sh
 
-printf 'Git - proceed configuration? (y/N)'
-read git_config
-if echo "$git_config" | grep -iq "^y" ;then
-    bash -x ./git-config.sh
-fi
-
-printf 'Yubikey - proceed configuration? (y/N)'
-read yubikey
-if echo "$yubikey" | grep -iq "^y" ;then
-    bash -x ./yubikey.sh
-fi
-
 printf 'RPM Fusion - proceed installation? (y/N)'
 read rpmfusion
 if echo "$rpmfusion" | grep -iq "^y" ;then
     bash -x ./rpm-fusion.sh
-fi
-
-printf 'Sublime - proceed installation? (y/N)'
-read sublime
-if echo "$sublime" | grep -iq "^y" ;then
-    bash -x ./sublime.sh
 fi
 
 printf 'Packages - proceed installation? (y/N)'
@@ -32,11 +14,33 @@ if echo "$packages" | grep -iq "^y" ;then
     bash -x ./packages.sh
 fi
 
+printf 'Sublime - proceed installation? (y/N)'
+read sublime
+if echo "$sublime" | grep -iq "^y" ;then
+    bash -x ./sublime.sh
+fi
+
 printf 'Flatpak - proceed installation? (y/N)'
 read flatpak
 if echo "$flatpak" | grep -iq "^y" ;then
     bash -x ./flatpak.sh
 fi
+
+printf 'Yubikey - proceed configuration? (y/N)'
+read yubikey
+if echo "$yubikey" | grep -iq "^y" ;then
+    bash -x ./yubikey.sh
+fi
+
+printf 'Git - proceed configuration? (y/N)'
+read git_config
+if echo "$git_config" | grep -iq "^y" ;then
+    bash -x ./git-config.sh
+fi
+
+
+
+
 
 
 . ~/.bashrc
