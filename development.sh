20b928399081f5342cd58a0ae2b50ff9369244e6
Claudio Walser <info@gitcd.io>
2025-06-01 09:12:51 +0200

    Update development.sh

--- development.sh
+++ development.sh
@@ -1,5 +1,4 @@
 set -o nounset # exit when trying to use undeclared variables
 
 ln -s /var/lib/flatpak/exports/bin/com.sublimetext.three $HOME/.local/bin/subl
-
-
+ln -s /var/lib/flatpak/exports/bin/com.vscodium.codium $HOME/.local/bin/vscode
