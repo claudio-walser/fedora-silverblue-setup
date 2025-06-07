8b4820cc7b54e28132a73e9903d3430bfcf963d0
Claudio Walser <info@gitcd.io>
2025-06-07 15:24:17 +0200

    Update development.sh

--- development.sh
+++ development.sh
@@ -6,3 +6,7 @@ usermod -aG dialout ${USER}
 
 ln -s /var/lib/flatpak/exports/bin/com.sublimetext.three $HOME/.local/bin/subl
 ln -s /var/lib/flatpak/exports/bin/com.vscodium.codium $HOME/.local/bin/vscode
+
+
+# vscode extensions
+# https://KalAster.gallery.vsassets.io/_apis/public/gallery/publisher/KalAster/extension/vscode-riotjs/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
