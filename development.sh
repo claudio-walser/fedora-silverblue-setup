531de366092f7deacfd858a88f5ccb39047f6b7c
Claudio Walser <info@gitcd.io>
2026-02-14 21:33:13 +0100

    Create .bashrc.d for Android SDK configuration
    Add Android SDK environment setup to .bashrc.d

--- development.sh
+++ development.sh
@@ -7,6 +7,14 @@ usermod -aG dialout ${USER}
 ln -s /var/lib/flatpak/exports/bin/com.sublimetext.three $HOME/.local/bin/subl
 ln -s /var/lib/flatpak/exports/bin/com.vscodium.codium $HOME/.local/bin/vscode
 
+# .bashrc.d to set android home
+mkdir -p ~/.bashrc.d
+if [ ! -f ~/.bashrc.d/android ]; then
+    echo "Writing pgp-ssh .bashrc.d file"
+    cat > ~/.bashrc.d/android <<EOL
+export ANDROID_HOME=/var/home/claudio/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+EOL
 
 # vscode extensions
 # https://KalAster.gallery.vsassets.io/_apis/public/gallery/publisher/KalAster/extension/vscode-riotjs/latest/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
